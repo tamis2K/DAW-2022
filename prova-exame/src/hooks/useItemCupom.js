766508644e76582eec2f97e8da6fd1295f05f11b
Mateus Varnier <90485488+tamis2K@users.noreply.github.com>
2022-12-18 20:14:03 -0300

    Delete prova-exame directory

--- prova-exame/src/hooks/useItemCupom.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { ItemCupomContext } from "../context/ItemCupomContext";
-
-export function useItemCupom() {
-    const context = useContext(ItemCupomContext);
-    return context;
-}
